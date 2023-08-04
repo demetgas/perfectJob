0bf4ff47ad250bf785693c6bab772b9294614324
demetgas <demet_gas@hotmail.com>
2023-08-04 17:35:42 +0200

    error fixed

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -20,9 +20,9 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
 
       <View style={styles.textContainer}>
         <Text style={styles.jobName} numberOfLines={1}>
-          {job.job_title}
+          {job?.job_title}
         </Text>
-        <Text style={styles.jobType}>{job.employment_type}</Text>
+        <Text style={styles.jobType}>{job?.employment_type}</Text>
       </View>
     </TouchableOpacity>
   );
