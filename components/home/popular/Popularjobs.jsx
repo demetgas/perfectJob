85b03507542c45627e04916c4bd59594a72be407
demetgas <demet_gas@hotmail.com>
2023-08-04 14:50:34 +0200

    added a flatlist

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,6 +13,8 @@ import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
   const router = useRouter();
+  const isLoading = false;
+  const error = false;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -22,7 +24,16 @@ const Popularjobs = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.cardsContainer}>
-
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong...</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+          />
+        )}
       </View>
     </View>
   );
