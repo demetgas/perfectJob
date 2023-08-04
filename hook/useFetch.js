2f5c6729794fd180ab73c3e3bc4dac6cdb8070ef
demetgas <demet_gas@hotmail.com>
2023-08-04 15:18:45 +0200

    code copied from rapidApi

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -5,4 +5,27 @@ const useFetch = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://jsearch.p.rapidapi.com/search',
+  params: {
+    query: 'Python developer in Texas, USA',
+    page: '1',
+    num_pages: '1'
+  },
+  headers: {
+    'X-RapidAPI-Key': '13b130a396msh47f3b0e072380efp1937f7jsnd0b3772e093e',
+    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
 };
