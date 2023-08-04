3673bd5077ea62563ebe256b3dae9606381c0df4
demetgas <demet_gas@hotmail.com>
2023-08-04 15:22:19 +0200

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,31 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetch = () => {
+const useFetch = (endpoint) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const axios = require('axios');
+  const axios = require("axios");
 
-const options = {
-  method: 'GET',
-  url: 'https://jsearch.p.rapidapi.com/search',
-  params: {
-    query: 'Python developer in Texas, USA',
-    page: '1',
-    num_pages: '1'
-  },
-  headers: {
-    'X-RapidAPI-Key': '13b130a396msh47f3b0e072380efp1937f7jsnd0b3772e093e',
-    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-  }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: {
+      query: "Python developer in Texas, USA",
+      page: "1",
+      num_pages: "1",
+    },
+    headers: {
+      "X-RapidAPI-Key": "13b130a396msh47f3b0e072380efp1937f7jsnd0b3772e093e",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
 };
