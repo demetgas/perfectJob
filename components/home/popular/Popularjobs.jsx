f85775a9c53a22595c09ee47e0ffb6fac39a3a79
demetgas <demet_gas@hotmail.com>
2023-08-04 22:29:28 +0200

    error fixed

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -20,6 +20,7 @@ const Popularjobs = () => {
   });
 
   const [selectedJob, setSelectedJob] = useState();
+
   const handleCardPress = (item) => {
     router.push(`/job-details/${item.job_id}`);
     setSelectedJob(item.job_id);
@@ -41,7 +42,13 @@ const Popularjobs = () => {
         ) : (
           <FlatList
             data={data}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
