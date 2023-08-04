d085e5972580307bd9c551633a171bffc03d1fd5
demetgas <demet_gas@hotmail.com>
2023-08-04 15:36:35 +0200

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -25,6 +25,8 @@ const useFetch = (endpoint, query) => {
     setIsLoading(true);
     try {
       const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
     } catch (error) {
     } finally {
     }
