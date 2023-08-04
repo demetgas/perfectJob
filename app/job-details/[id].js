1dbfeff2a1104d21b0adea59e3d29c287e812040
demetgas <demet_gas@hotmail.com>
2023-08-04 19:11:17 +0200

    Update [id].js

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,7 +20,15 @@ import {
 } from "../../components";
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
+
 const JobDetails = () => {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const [data, isLoading, error, refetch] = useFetch("job-details", {
+    job_id: params.id,
+  });
+
   return <Text>JobDetails</Text>;
 };
 
