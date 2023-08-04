74147725a568ae5fce40f6517ea352d0ea986ed3
demetgas <demet_gas@hotmail.com>
2023-08-04 22:03:57 +0200

    Update Welcome.jsx

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -14,7 +14,7 @@ import { icons, SIZES } from "../../../constants";
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
   const [activeJobType, setActiveJobType] = useState("Full-Time");
 
@@ -28,7 +28,7 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=""
+            value={searchTerm}
             onChange={() => {}}
             placeholder="What are you looking for?"
           />
