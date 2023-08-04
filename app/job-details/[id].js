f41f0e2250fbed41c370479e53c3397c4c7fe594
demetgas <demet_gas@hotmail.com>
2023-08-04 19:06:32 +0200

    Update [id].js

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Text } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
 const JobDetails = () => {
   return <Text>JobDetails</Text>;
 };
