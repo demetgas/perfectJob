827d0a91e24226503d1b2686f6aa9aabe44e34e8
demetgas <demet_gas@hotmail.com>
2023-08-04 17:16:56 +0200

    Update Nearbyjobs.jsx

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
-import styles from "./popularjobs.style";
-import { COLORS, SIZES } from "../../../constants";
-import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import styles from "./nearbyjobs.style";
+import { COLORS } from "../../../constants";
+import NeaerbyJobCard from "../../common/cards/nearby/NearbyJobCard";
 import useFetch from "../../../hook/useFetch";
 
-const Popularjobs = () => {
+const Nearbyjobs = () => {
   const router = useRouter();
   const { data, isLoading, error } = useFetch("search", {
     query: "React developer",
@@ -22,7 +15,7 @@ const Popularjobs = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
         <TouchableOpacity>
           <Text style={styles.headerBtn}>Show All</Text>
         </TouchableOpacity>
@@ -33,17 +26,11 @@ const Popularjobs = () => {
         ) : error ? (
           <Text>Something went wrong...</Text>
         ) : (
-          <FlatList
-            data={data}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
-            keyExtractor={(item) => item?.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal
-          />
+          data?.map((job) => <NeaerbyJobCard />)
         )}
       </View>
     </View>
   );
 };
 
-export default Popularjobs;
+export default Nearbyjobs;
